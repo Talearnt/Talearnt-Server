068417dd089de6dd82df8f05e0ff43254bfce03d
Ryu_woong <jin02014@naver.com>
2024-10-30 01:50:32 +0900

    feat : Add Community Post Controller
    - 폴더 구조를 명확히 하기 위해 미리 추가
    
    Related to : TALEARNT-110

---
+++ src/main/java/com/talearnt/post/community/CommunityPostController.java
@@ -0,0 +1,9 @@
+package com.talearnt.post.community;
+
+import com.talearnt.examples.RestControllerV1;
+import lombok.extern.log4j.Log4j2;
+
+@RestControllerV1
+@Log4j2
+public class CommunityPostController {
+}
