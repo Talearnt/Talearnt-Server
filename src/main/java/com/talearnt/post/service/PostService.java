8d070b925ab4952f56cb8d4de32878a6502670c8
Ryu_woong <jin02014@naver.com>
2024-10-30 16:55:59 +0900

    feat : Update Exchange Classes
    - Exchage post Entity  id Type int -> long으로 변경
    - service Method 추가
    
    Related to : TALEARNT-113, TALEARNT-110

--- src/main/java/com/talearnt/post/service/PostService.java
+++ src/main/java/com/talearnt/post/service/PostService.java
@@ -1,4 +1,8 @@
 package com.talearnt.post.service;
 
+import com.talearnt.util.response.CommonResponse;
+import org.springframework.http.ResponseEntity;
+
 public interface PostService {
+    ResponseEntity<CommonResponse> addPost();
 }
--- src/main/java/com/talearnt/post/service/impl/ExchangePostServiceImpl.java
+++ src/main/java/com/talearnt/post/service/impl/ExchangePostServiceImpl.java
@@ -1,6 +1,14 @@
 package com.talearnt.post.service.impl;
 
 import com.talearnt.post.service.PostService;
+import com.talearnt.util.response.CommonResponse;
+import org.springframework.http.ResponseEntity;
 
 public class ExchangePostServiceImpl implements PostService {
+
+    @Override
+    public ResponseEntity<CommonResponse> addPost() {
+        return null;
+    }
+
 }
