d918e1cb0ee1b4197f8f4b40322892c449ec43bd
Sanghoon <thsrang@gmail.com>
2024-10-31 17:55:46 +0900

    feat : Add addFilterBefore
    - SecurityConfig에 jwtFilter 추가
    
    Related to : TALEARNT-84

--- src/main/java/com/talearnt/configure/SecurityConfig.java
+++ src/main/java/com/talearnt/configure/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.talearnt.configure;
 
-//import com.talearnt.Login.JwtFilter;
+import com.talearnt.util.jwt.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,15 +11,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // 비밀번호를 인코딩하기 위한 기본 인코더
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,8 +38,9 @@ public class SecurityConfig {
 
         /* Resolves 상태로 Github에 업로드하지 않아 Login 관련 JWT Filter가 없습니다.
         * Login 관련도 완벽히 완성될 경우에 해당 주석과 밑 주석을 제거하고 import 부분 주석도 삭제해주시길 바랍니다. */
-        //http.addFilterBefore(new JwtFilter(), ExceptionTranslationFilter.class);
-
+        // JwtFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        // 권한 설정 어떻게 분기 시킬지 논의 필요
         http.authorizeHttpRequests((authorize) ->
                 authorize.requestMatchers("/**").permitAll()
         );
