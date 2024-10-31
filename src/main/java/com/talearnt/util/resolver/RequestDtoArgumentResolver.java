6e6442883d109da2d6b2720f9164bd79d7b75120
Ryu_woong <jin02014@naver.com>
2024-10-31 20:43:22 +0900

    feat : Add JWT -> DTO 자동 정보 주입
    - JWT 정보가 필요한  DTO 정보 자동 주입 생성

--- src/main/java/com/talearnt/util/resolver/RequestDtoArgumentResolver.java
+++ src/main/java/com/talearnt/util/resolver/RequestDtoArgumentResolver.java
@@ -1,11 +1,14 @@
 package com.talearnt.util.resolver;
 
 import com.talearnt.util.common.RequestDTO;
+import com.talearnt.util.jwt.UserInfo;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.lang.reflect.Field;
 
 public class RequestDtoArgumentResolver implements HandlerMethodArgumentResolver {
 
@@ -21,22 +24,18 @@ public class RequestDtoArgumentResolver implements HandlerMethodArgumentResolver
         // DTO 인스턴스 생성
         Object dto = parameter.getParameterType().getDeclaredConstructor().newInstance();
 
-        // 현재 요청의 UserDetails를 가져옴
-        /* User Details 정의하면 그때 주석 삭제 후 적용.
-        CustomDetails userInfo = (CustomDetails) webRequest.getUserPrincipal();
+        UserInfo userInfo = (UserInfo) webRequest.getUserPrincipal();
 
         //JWT가 Null이 아니라면
         if (userInfo != null) {
             // DTO의 필드 중 "userId"가 있는 경우 CustomDetails의 userId 값 주입
-            for (Field field : dtoInstance.getClass().getDeclaredFields()) {
+            for (Field field : dto.getClass().getDeclaredFields()) {
                 if (field.getName().equals("userId")) {
                     field.setAccessible(true);
                     field.set(dto, userInfo.getUserId());
                 }
             }
         }
-
-         */
 
         return dto;
     }
