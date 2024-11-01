9c1b07cff01a510bf4513b7f99887b11864e6768
Woong <jin02014@naver.com>
2024-11-01 15:25:58 +0900

    feat : Update Read,Update,Create Exchange Post DTO
    -  Read Response DTO 설정

---
+++ src/main/java/com/talearnt/post/exchange/response/ExchangePostReadResDTO.java
@@ -0,0 +1,34 @@
+package com.talearnt.post.exchange.response;
+
+import com.talearnt.enums.UserRole;
+import com.talearnt.enums.post.ExchangeType;
+import com.talearnt.post.exchange.PostTalentCategoryDTO;
+import com.talearnt.util.jwt.UserInfo;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExchangePostReadResDTO {
+    //게시글 번호
+    private long exchangePostNo;
+    //이하 유저 관련
+    private long userNo;
+    private String userId;
+    private String nickname;
+    private String profileImg;
+    private UserRole authority;
+
+    //이하 게시글 관련
+    private List<PostTalentCategoryDTO> giveTalent;
+    private List<PostTalentCategoryDTO> receiveTalent;
+    private String title;
+    private String content;
+    private ExchangeType exchangeType;
+    private boolean badgeRequired;
+    private String duration;
+}
