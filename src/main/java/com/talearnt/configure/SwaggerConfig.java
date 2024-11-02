1b065e1495440d5cb64ed8e170b7a897953c68f3
Woong <jin02014@naver.com>
2024-11-02 17:43:39 +0900

    feat : Update Swagger config - JWT로 테스트 할 수 있도록 수정
    - JWT로 테스트 할 수 있도록 Swagger 설정
    - Login Request DTO Schema 설정

--- src/main/java/com/talearnt/configure/SwaggerConfig.java
+++ src/main/java/com/talearnt/configure/SwaggerConfig.java
@@ -4,6 +4,8 @@ import com.talearnt.enums.ErrorCode;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +14,17 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
 
-        Components components = new Components();
+        // JWT 보안 사용 설정
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("bearerAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        // JWT 사용 등록
+        Components components = new Components().addSecuritySchemes("bearerAuth", securityScheme);
         OpenAPI openApi = new OpenAPI().components(components)
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .info(apiInfo());
 
         for (ErrorCode errorCode : ErrorCode.values()){
@@ -26,8 +37,9 @@ public class SwaggerConfig {
     private Info apiInfo() {
         return new Info()
                 .title("Talearnt API")
-                .description("Talearnt 개발자의 API 소통구간")
+                .description("Talearnt 개발자의 API 소통구간 \n JWT 토큰 발급 ID test@test.com\n PWD : test")
                 .version("1.0.0");
+
     }
 
 }
--- src/main/java/com/talearnt/login/LoginReqDTO.java
+++ src/main/java/com/talearnt/login/LoginReqDTO.java
@@ -1,5 +1,6 @@
 package com.talearnt.login;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Component;
 @ToString
 public class LoginReqDTO {
 
+    @Schema(example = "test@test.com")
     private String userId;
+    @Schema(example = "test")
     private String pw;
 
 }
