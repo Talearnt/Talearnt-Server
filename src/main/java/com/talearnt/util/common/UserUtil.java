f595940f86641c36af8cb96cd08027f77a43066b
Ryu_woong <jin02014@naver.com>
2024-11-04 02:03:12 +0900

    feat : Add UserUtill - 손 쉽게 UserEntity 채울 수 있도록 변경
    - forigner key를 설정한 곳을 쉽게 사용할 수 있도록  UserUtil 생성

--- src/main/java/com/talearnt/util/common/UserUtil.java
+++ src/main/java/com/talearnt/util/common/UserUtil.java
@@ -1,12 +1,15 @@
 package com.talearnt.util.common;
 
 import com.talearnt.join.User;
+import com.talearnt.util.jwt.UserInfo;
 
 public class UserUtil {
 
-    public static User createUser(Long userNo){
+    public static User createUser(UserInfo userInfo){
         User user = new User();
-        user.setUserNo(userNo);
+        user.setUserNo(userInfo.getUserNo());
+        user.setNickname(userInfo.getNickname());
+        user.setAuthority(userInfo.getAuthority());
         return user;
     }
 }
