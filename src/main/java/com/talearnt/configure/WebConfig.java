b020970133faaf3feb2daf638094ec222a0083a6
Ryu_woong <jin02014@naver.com>
2024-11-03 20:05:16 +0900

    feat : Update WebConfig
    - Localhost : 5173 등록
    - Localhost:5173 으로 접근 시 아이피가 변경되어도 확인 가능한 지 확인

--- src/main/java/com/talearnt/configure/WebConfig.java
+++ src/main/java/com/talearnt/configure/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://121.168.173.15:5173, http://localhost:3000") // 허용할 도메인
+                .allowedOrigins("http://121.168.173.15:5173","http://localhost:5173") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true) // 쿠키를 포함한 자격 증명 허용
-                .maxAge(3600); // 캐싱 시간 (초)
+                .maxAge(36000); // 캐싱 시간 (초)
     }
 }
