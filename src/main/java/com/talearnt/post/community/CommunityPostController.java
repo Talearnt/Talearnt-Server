880ce3e8efb210db4f8fc128f003eb9f977e4409
Ryu_woong <jin02014@naver.com>
2024-11-04 04:41:01 +0900

    feat : Update Controller & ServiceImpl - CRUD Service 적용
    - 확장성 향상할 수 있도록 CRUD Service 적용
    - 책임이 직관적으로 변했습니다.
    
    Resolves : TALEARNT-113

--- src/main/java/com/talearnt/post/community/CommunityPostController.java
+++ src/main/java/com/talearnt/post/community/CommunityPostController.java
@@ -1,9 +1,12 @@
 package com.talearnt.post.community;
 
 import com.talearnt.examples.RestControllerV1;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestControllerV1
 @Log4j2
+@RequiredArgsConstructor
 public class CommunityPostController {
+
 }
--- src/main/java/com/talearnt/post/service/ExchangePostServiceImpl.java
+++ src/main/java/com/talearnt/post/service/ExchangePostServiceImpl.java
@@ -1,10 +1,9 @@
 package com.talearnt.post.service;
 
-import com.talearnt.join.User;
 import com.talearnt.post.exchange.request.ExchangePostCreateReqDTO;
 import com.talearnt.post.exchange.ExchangePostRepository;
 import com.talearnt.post.exchange.entity.ExchangePost;
-import com.talearnt.post.exchange.response.ExchangePostReadResDTO;
+import com.talearnt.post.exchange.request.ExchangePostUpdateReqDTO;
 import com.talearnt.util.common.UserUtil;
 import com.talearnt.util.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
@@ -17,30 +16,35 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Log4j2
-public class ExchangePostServiceImpl implements PostService {
+public class ExchangePostServiceImpl implements PostService<ExchangePostCreateReqDTO,ExchangePostUpdateReqDTO>{
 
     private final ExchangePostRepository exchangePostRepository;
     private final ModelMapper mapper;
 
 
     @Override
-    public ResponseEntity<CommonResponse<String>> createPost(ExchangePostCreateReqDTO exchangePostReqDTO) {
-        log.info("DTO 안 UserInfo 값 : {}",exchangePostReqDTO.getUserInfo());
-        log.info("DTO 값 : {}",exchangePostReqDTO);
+    public ResponseEntity<CommonResponse<String>> create(ExchangePostCreateReqDTO createDTO) {
+
+        log.info("DTO 안 UserInfo 값 : {}",createDTO.getUserInfo());
+        log.info("DTO 값 : {}",createDTO);
 
         //DTO -> Server Data로 변환
-        ExchangePost entity = mapper.map(exchangePostReqDTO,ExchangePost.class);
+        ExchangePost entity = mapper.map(createDTO,ExchangePost.class);
         log.info("DTO -> Entity로 변환된 값 : {}",entity);
 
         //User Entity에 UserNo 설정
-        entity.setUser(UserUtil.createUser(exchangePostReqDTO.getUserInfo()));
+        entity.setUser(UserUtil.createUser(createDTO.getUserInfo()));
 
         log.info("UserNo 주입 후 값 : {}",entity);
 
         //Data 저장
         exchangePostRepository.save(entity);
 
-
         return CommonResponse.success("재능 교환 게시글을 등록했습니다.");
+    }
+
+    @Override
+    public ResponseEntity<CommonResponse<String>> update(ExchangePostUpdateReqDTO updateDTO) {
+        return null;
     }
 }
