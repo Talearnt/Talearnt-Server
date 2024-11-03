0fdb6cc71c0c40869109f761653126b8df8e197c
Ryu_woong <jin02014@naver.com>
2024-11-03 19:46:01 +0900

    feat : Add WebConfig - CORS 문제 해결
    - 도메인을 사기 전까지 지속적인 아이피 변경 필요

---
+++ src/main/java/com/talearnt/configure/WebConfig.java
@@ -0,0 +1,19 @@
+package com.talearnt.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("http://121.168.173.15:5173, http://localhost:3000") // 허용할 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true) // 쿠키를 포함한 자격 증명 허용
+                .maxAge(3600); // 캐싱 시간 (초)
+    }
+}
