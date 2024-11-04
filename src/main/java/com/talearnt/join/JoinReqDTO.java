55420d137c9355a2a5c338928f1af2605fe1d66f
SH_Edw <thsrang@gmail.com>
2024-11-05 00:13:45 +0900

    style : Add Schema annotation
    - ReqDTO 에 Schema 추가
    
    Related to : TALEARNT-37

--- src/main/java/com/talearnt/join/JoinReqDTO.java
+++ src/main/java/com/talearnt/join/JoinReqDTO.java
@@ -10,13 +10,25 @@ import lombok.Setter;
 @Getter
 @Builder
 public class JoinReqDTO {
-    @Schema(required = true, example = "example.com")
+
+    @Schema(required = true, example = "example@example.com")
     private String userId;
+
+    @Schema(required = true, description = "8자 이상, 숫자,문자, 특수기호 각 1개 이상 포함")
     private String pw;
-    @Schema(description = "닉네임을 정제하여 넣어주세요.")
+
+    @Schema(description = "랜덤 닉네임 자동 주입")
     private String nickname;
+
+    @Schema(required = true, example = "남자/여자")
     private Gender gender;
+
+    @Schema(required = true, example = "01012345678")
     private String phone;
+
+    @Schema(description = "회원가입 경로에 따라 자동 주입 자사/카카오")
     private String joinType;
+
+    @Schema(description = "ROLE_USER 기본값 자동 주입")
     private UserRole authority;
 }
--- src/main/java/com/talearnt/verification/VerificationReqDTO.java
+++ src/main/java/com/talearnt/verification/VerificationReqDTO.java
@@ -1,5 +1,6 @@
 package com.talearnt.verification;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +9,16 @@ import lombok.Setter;
 @Setter
 @Builder
 public class VerificationReqDTO {
+
+    @Schema(required = true, example = "example@example.com")
     private String userId;
+
+    @Schema(required = true, example = "01012345678")
     private String phone;
+
+    @Schema(description = "서버에서 4자리 인증번호 생성")
     private String verificationCode;
+
+    @Schema(description = "default = false, 인증 후 true")
     private Boolean isPhoneVerified;
 }
