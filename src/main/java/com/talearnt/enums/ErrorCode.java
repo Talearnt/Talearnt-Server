2d96746fea170e890dfc561da772db3cb3138b35
Woong <jin02014@naver.com>
2024-11-04 17:32:15 +0900

    feat : Update Error Code and Exception Handler
    - Exception 양식 이상하게 떠서 수정

--- src/main/java/com/talearnt/enums/ErrorCode.java
+++ src/main/java/com/talearnt/enums/ErrorCode.java
@@ -12,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
     // 인증 관련 오류
     INVALID_TOKEN("401-AUTH-01", "올바르지 않은 인증 토큰입니다."),
-    EXPIRED_TOKEN("401-AUTH-02", "인증 토큰이 만료되었습니다."),
+    EXPIRED_TOKEN("401-AUTH-02", "로그인 후 다시 이용해주세요."),
     INVALID_CREDENTIALS("401-AUTH-03", "아이디 또는 비밀번호를 확인해주세요!"),
     ACCESS_DENIED("403-AUTH-04", "접근이 거부되었습니다."),
     INVALID_AUTH_CODE("400-AUTH-05","인증 번호가 틀립니다. 확인 후 다시 입력해주세요"),
@@ -52,15 +52,17 @@ public enum ErrorCode {
     POST_CONTENT_MISSING("400-POST-03", "내용을 입력해주세요."),
     POST_CONTENT_MIN_LENGTH("400-POST-04", "내용 20글자 이상 필수 입력입니다!"),
     POST_REQUEST_MISSING("400-POST-05", "필수 항목을 입력해 주세요."),
-    POST_BAD_REQUEST("400-POST-06", "잘못된 값이 넘어왔습니다. \n 입력 값을 확인하세요."),
-    POST_OVER_REQUEST_LENGTH("400-POST-06", "최대 5개의 재능만 선택하실 수 있습니다"),
+    POST_BAD_REQUEST("400-POST-06", "잘못된 값이 넘어왔습니다. 입력 값을 확인하세요."),
+    POST_OVER_REQUEST_LENGTH("400-POST-07", "최대 5개의 재능만 선택하실 수 있습니다"),
+    POST_NOT_FOUND("404-POSET-08", "해당 게시글을 찾을 수 없습니다."),
+
 
     //서버 오류
-    ILLEGAL_ARGUMENT_EXCEPTION("400-SERVER-01", "잘못된 값을 입력하셨습니다. \n 입력 값을 확인하세요."),
+    ILLEGAL_ARGUMENT_EXCEPTION("400-SERVER-01", "잘못된 값을 입력하셨습니다. 입력 값을 확인하세요."),
 
     //이미 만들어져 공통으로 사용되는 오류
     METHOD_NOT_SUPPORTED("405-COMMON-01", "지원하지 않는 HTTP 메서드입니다."),
-    RESOURCE_NOT_FOUND("404-COMMON-02", "요청한 리소스를 찾을 수 없습니다. \n 경로를 확인하세요.");
+    RESOURCE_NOT_FOUND("404-COMMON-02", "요청한 리소스를 찾을 수 없습니다. 경로를 확인하세요.");
 
 
     private final String code;
