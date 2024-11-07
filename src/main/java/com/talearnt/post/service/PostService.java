29b3f91d64ace044279246685cf2f2c4a61cd750
Ryu_woong <jin02014@naver.com>
2024-11-08 04:39:00 +0900

    feat : Add ListService And Update ErrorCode, PostService
    - ListService : 목록 조회는 강제로 PaginatedResponse를 구현하도록 만들었습니다.
    - ErrorCode :  page 번호가 0 이하 , 최대 페이지 이상일 경우를 추가하였습니다.
    - PostService : ListService를 적용하여 Impl에 강제하였습니다.
    
    Related to : TALEARNT-111

--- src/main/java/com/talearnt/post/service/PostService.java
+++ src/main/java/com/talearnt/post/service/PostService.java
@@ -1,13 +1,13 @@
 package com.talearnt.post.service;
 
+import com.talearnt.post.exchange.response.ExchangePostListResDTO;
 import com.talearnt.post.exchange.response.ExchangePostReadResDTO;
-import com.talearnt.service.CreateService;
-import com.talearnt.service.DeleteService;
-import com.talearnt.service.ReadService;
-import com.talearnt.service.UpdateService;
+import com.talearnt.service.*;
 
 public interface PostService<T> extends CreateService<T, String>
         , UpdateService<T, String>
-        , ReadService<Long, ExchangePostReadResDTO>, DeleteService<Long,String> {
+        , ReadService<Long, ExchangePostReadResDTO>
+        , DeleteService<Long,String>
+        , ListService<Integer, ExchangePostListResDTO> {
 
 }
