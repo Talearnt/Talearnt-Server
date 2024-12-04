057c37c3b8081a5bfc65dd835a2ceb0141a55221
Woong <jin02014@naver.com>
2024-12-04 21:52:25 +0900

    docs : Update Swagger 문서화 개선
    - Api Interface를 만들고 구현하여 문서와 Controller를 분리하였습니다.
    
    Resolves : #TALEARNT-241

--- src/main/java/com/talearnt/auth/AuthApi.java
+++ src/main/java/com/talearnt/auth/AuthApi.java
@@ -29,20 +29,39 @@ import org.springframework.web.bind.annotation.*;
 
 public interface AuthApi {
     /*000000000000000000000000000000 회원가입 관련 시작 000000000000000000000000000000*/
-    @Operation(summary = "아이디 중복 확인 - 중복O True, 중복X False")
+    @Operation(summary = "아이디 중복 확인",
+    description = "<p>아이디 중복을 체크하는 API입니다.</p>" +
+            "<ul>" +
+                "<li>true : 중복임</li>" +
+                "<li>false : 중복 아님</li>" +
+            "</ul>")
     @ApiResponse(responseCode = "400", ref = "USER_ID_NOT_EMAIL_FORMAT")
     public ResponseEntity<CommonResponse<Boolean>> checkDuplicatedUserID(@PathVariable @DynamicValid(errorCode = ErrorCode.USER_ID_NOT_EMAIL_FORMAT, pattern = Regex.EMAIL)
                                                                          String userId);
 
-    @Operation(summary = "닉네임 중복 확인 - 중복O True, 중복X False")
+    @Operation(summary = "닉네임 중복 확인",
+            description = "<p>닉네임 중복을 체크하는 API입니다.</p>" +
+            "<ul>" +
+                "<li>true : 중복임</li>" +
+                "<li>false : 중복 아님</li>" +
+            "</ul>")
     public ResponseEntity<CommonResponse<Boolean>> checkDuplicatedNickname(@PathVariable @DynamicValid(errorCode = ErrorCode.DUPLICATE_USER_NICKNAME,pattern = Regex.NICKNAME)
                                                                            String nickname);
 
-    @Operation(summary = "랜덤한 닉네임 생성, 회원가입 닉네임 입력창으로 이동 시 호출")
+    @Operation(summary = "랜덤한 닉네임 생성, 회원가입 닉네임 입력창으로 이동 시 호출",
+            description = "<h3>랜덤한 닉네임을 생성합니다. 중복 검사를 거친 닉네임입니다.</h3>" +
+                    "<h4>랜덤 닉네임 구성은 아래와 같습니다</h4>" +
+                    "<p>형용사(4글자) + 명사(2~3글자) + # + 1~100</p>" +
+                    "<p>최대 11자로 생성됩니다.</p>" +
+                    "<p>기획서에 입력된 내용처럼 닉네임은 최소 2자~12자로 보내주셔야 합니다.</p>")
     public ResponseEntity<CommonResponse<String>> MakeRandomNickname();
 
     @Operation(summary = "휴대폰 인증까지 마치고 회원 가입 완료 단계, 자사 회원 가입",
-            description = "회원가입 인증과 모든 것을 마치고 회원가입 완료를 눌렀을 때 호출<br> 필수 이용약관은 true로 보내셔야 합니다.")
+            description = "<h2>내용</h2>\n" +
+                    "<p>휴대폰 번호 인증이 완료된 후에 가입이 가능합니다.</p>\n" +
+                    "<hr>\n" +
+                    "<p>필수 이용 약관은 true로 보내야합니다.</p>\n" +
+                    "<p>이용 약관 코드는 <code>관리자 2. 이용약관</code> 에서 활성화된 이용 약관을 호출하여 설정해주시길 바랍니다.</p>")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "회원가입 성공"),
             @ApiResponse(responseCode = "400-1", ref = "UNVERIFIED_AUTH_CODE"),
@@ -61,7 +80,10 @@ public interface AuthApi {
     public ResponseEntity<CommonResponse<String>> addUser(@RequestBody @Valid JoinReqDTO joinReqDTO) throws CustomException;
 
 
-    @Operation(summary = "카카오톡 회원가입",description = "필수 이용약관은 true로 보내셔야 합니다.")
+    @Operation(summary = "카카오톡 회원가입",
+            description = "<h2>내용</h2>\n" +
+                    "<p>필수 이용 약관은 true로 보내야합니다.</p>\n" +
+                    "<p>이용 약관 코드는 <code>관리자 2. 이용약관</code> 에서 활성화된 이용 약관을 호출하여 설정해주시길 바랍니다.</p>")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "카카오톡 회원가입 성공"),
             @ApiResponse(responseCode = "400-1", ref = "UNVERIFIED_AUTH_CODE"),
@@ -78,7 +100,10 @@ public interface AuthApi {
     /*000000000000000000000000000000 회원가입 관련 끝 000000000000000000000000000000*/
     /*############################## 로그인 관련 시작 ##############################*/
     @Operation(summary = "로그인 요청",
-            description = "로그인 성공 시 jwt 발급, 실패시 error <br> Refresh 토큰은 SameSite 문제로 설정되지 않고 있는 것 같습니다. AccessToken으로 확인하시길 바랍니다.",
+            description = "<h2>내용</h2>\n" +
+                    "<p>로그인 성공 시 JWT 토큰 발급</p>\n" +
+                    "<p>Refresh 토큰은 SameStie 문제로 설정되지 않고 있습니다.</p>\n" +
+                    "<p>SSL 인증서를 설치하여 HTTPS로 넘어간뒤 Refresh 토큰 적용을 시작하겠습니다.</p>",
             responses = {
                     @ApiResponse(responseCode = "200", description = "로그인 성공"),
                     @ApiResponse(responseCode = "404", ref = "USER_NOT_FOUND"),
@@ -95,12 +120,23 @@ public interface AuthApi {
             })
     public ResponseEntity<CommonResponse<TokenResDTO>> refresh(@CookieValue("refreshToken") String refreshToken) throws CustomException;
 
-    @Operation(summary = "카카오톡 로그인", description = "로그인 했는데 값이 없을 경우 isRequiredRedirect가 true 가 됩니다.<br>" +
-            "userId,gender,phone값이 넘어갑니다. 랜덤 닉네임 생성 API 호출 해야합니다. <br> 로그인 시 선택 사항인 gender와 phone을 선택 안했으면 null로 넘어갑니다.<br>" +
-            "성공시 isRequiredRedirect : false와 accessToken : 값, cookie에는 Refresh 토큰이 셋팅됩니다.<br>" +
-            "카카오톡 로그인 URL : https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={REST_API_KEY}&redirect_uri={redirectURL}<br>" +
-            "REST_API_KEY : 카카오톡 developers 참고 <br>" +
-            "redirectURL : http://localhost/v1/auth/login/kakao (테스트 전용 URL)")
+    @Operation(summary = "카카오톡 로그인", description = "<p>카카오톡 로그인시 첫 로그인일 경우 회원가입 페이지로 넘어갑니다.</p>" +
+            "<p>Data에 들어가는 값</p>" +
+            "<ul>" +
+            "    <li>isRequiredRedirect : true - 회원가입</li>" +
+            "    <li>userId : 카카오톡 이메일</li>" +
+            "    <li>gender : 남자</li>" +
+            "    <li>phone : 카카오톡에 등록된 휴대폰 번호</li>" +
+            "</ul>" +
+            "<p>닉네임은 랜덤 닉네임 생성 API를 호출해주셔야 합니다.</p>" +
+            "<hr/>" +
+            "<p>카카오톡으로 로그인이 성공했을 경우에는 성공적으로 정상적으로 AccessToken이 응답 값으로 넘어갑니다.</p>" +
+            "<p>Refresh Token은 Cookie에 셋팅됩니다. (현재 SameSite 문제로 적용 안되고 있음)</p>" +
+            "<hr/>" +
+            "<p><strong>카카오톡 로그인 URL :</strong> https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={REST_API_KEY}&redirect_uri={redirectURL} <p>" +
+            "<p><strong>REST_API_KEY :</strong> 카카오톡 developers 참고<p>" +
+            "<p><strong>redirectURL :</strong> http://localhost/v1/auth/login/kakao (테스트 전용 URL)<p>" +
+            "")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200"),
             @ApiResponse(responseCode = "400", ref = "AUTH_METHOD_CONFLICT")
@@ -127,7 +163,29 @@ public interface AuthApi {
     })
     public ResponseEntity<CommonResponse<String>> sendAuthenticationCode(@RequestBody @Valid FindByPhoneReqDTO phoneReqDTO, HttpServletRequest request);
 
-    @Operation(summary = "인증 번호 문자 검증 통합",description = "<h3>type에 들어갈 수 있는 값 : '회원가입','아이디찾기'</h3>회원가입 Return : true/false <br> 아이디 찾기 Return : {<br>userId:string,<br>createdAt:string<br>} ")
+    @Operation(summary = "인증 번호 문자 검증 통합",
+            description = "<h2>인증 번호 검증 Type에 따라서 나오는 결과물이 달라집니다.</h2>" +
+                    "<p>`signUp` (회원가입)</p>" +
+                    "<pre>" +
+                    "{<br>" +
+                    "   success: true,<br>" +
+                    "   data : true,<br>" +
+                    "   errorCode : null,<br>" +
+                    "   errorMessage : null<br>" +
+                    "}<br>" +
+                    "</pre>" +
+                    "<p>`findId` (아이디 찾기)</p>" +
+                    "<pre>" +
+                    "{<br>" +
+                    "   success: true,<br>" +
+                    "   data : {<br>" +
+                    "               userId : string,<br>" +
+                    "               createdAt : string<br>" +
+                    "           }<br>" +
+                    "   errorCode : null,<br>" +
+                    "   errorMessage : null<br>" +
+                    "}<br>" +
+                    "</pre>")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "true"),
             @ApiResponse(responseCode = "429", ref = "AUTH_TOO_MANY_REQUEST"),
@@ -143,7 +201,15 @@ public interface AuthApi {
 
     /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 인증 문자 관련 끝 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
     /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 비밀번호 찾기 시작 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
-    @Operation(summary = "비밀번호 찾기 1. 비밀번호 이메일 전송", description = "일치하는 아이디와 휴대폰 번호가 있는지 검증하고, 있으면 해당 이메일로 비밀번호 변경 주소 보냅니다.")
+    @Operation(summary = "비밀번호 찾기 1. 비밀번호 이메일 전송",
+            description = "<h2>설명</h2>" +
+                    "<p>아이디와 휴대폰 번호가 일치하는 회원이 있을 경우 이메일이 전송됩니다.</p>" +
+                    "<hr/>" +
+                    "<h3>수정 예정</h3>" +
+                    "<p>각 이메일마다 <strong>style</strong> 적용 및 <strong>javascript</strong> 적용 기준이 다른 것을 확인했습니다.</p>" +
+                    "<p>Gmail 에서는 style이 제대로 적용되지 않습니다.</p>" +
+                    "<p>naver에서는 style이 적용됩니다.</p>" +
+                    "<p>이메일에서 스타일이 파괴되지 않는 규칙을 찾아 수정하겠습니다.</p>")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200"),
             @ApiResponse(responseCode = "400-1", ref = "USER_ID_NOT_EMAIL_FORMAT"),
@@ -156,7 +222,18 @@ public interface AuthApi {
                                                                                   @DynamicValid(errorCode = ErrorCode.USER_ID_NOT_EMAIL_FORMAT,pattern = Regex.EMAIL)
                                                                                   String userId) throws MessagingException;
 
-    @Operation(summary = "비밀번호 찾기 2. 비밀번호 변경", description = "10분 내로 인증하지 않으면 변경 불가, No와uuid를 param으로 받고 있습니다.<br> FE는 URL에서 얻을 수 있습니다.<br> Flutter는 테스트가 어렵습니다.<br> 이메일 전송까지가 완성으로 하겠습니다.")
+    @Operation(summary = "비밀번호 찾기 2. 비밀번호 변경",
+            description = "<h2>내용</h2>\n" +
+                    "<p>10분 이내로 비밀번호를 변경하지 않을 경우 변경이 불가합니다.</p>\n" +
+                    "<p><code>no</code>와<code>uuid</code>를 Path로 보내주시면 됩니다.</p>\n" +
+                    "<h3>Body</h3>\n" +
+                    "<ul>\n" +
+                    "    <li>pw: 변경할 비밀번호</li>\n" +
+                    "    <li>checkedPw: 변경할 비밀번호 확인</li>\n" +
+                    "</ul>\n" +
+                    "<hr/>\n" +
+                    "<p>Flutter는 이메일에서 변경하는 것이 어려울 것으로 판단됩니다.</p>\n" +
+                    "<p>모바일은 이메일 전송까지가 완성으로 하겠습니다.</p>")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200"),
             @ApiResponse(responseCode = "400-1", ref = "USER_PASSWORD_PATTERN_MISMATCH"),
