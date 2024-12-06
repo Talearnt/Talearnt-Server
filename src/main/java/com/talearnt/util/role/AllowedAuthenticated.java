971804efbbc4d590e84cc7545580a5d4df015d46
Woong <jin02014@naver.com>
2024-12-06 17:00:48 +0900

    feat : Add Role 어노테이션 생성
    - Controller에서 권한 설정을 하기 위하여 생성

---
+++ src/main/java/com/talearnt/util/role/AllowedAuthenticated.java
@@ -0,0 +1,14 @@
+package com.talearnt.util.role;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('AUTHENTICATED','MANAGER','ADMIN')")
+public @interface AllowedAuthenticated {
+}
