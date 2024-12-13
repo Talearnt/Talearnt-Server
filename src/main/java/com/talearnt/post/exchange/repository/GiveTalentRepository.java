ee8bdbd46a1267b976d3f67bbf6a0d40539cfdd6
Woong <jin02014@naver.com>
2024-12-13 15:29:21 +0900

    feat : Add 재능 게시글 등록 완료
    - N+1 문제 해결 ( 데이터 캐싱 )
    - Bulk Insert 구현 ( JDBC Template)
    
    Resolves : #TALEARNT-183

---
+++ src/main/java/com/talearnt/post/exchange/repository/GiveTalentRepository.java
@@ -0,0 +1,7 @@
+package com.talearnt.post.exchange.repository;
+
+import com.talearnt.post.exchange.entity.GiveTalent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GiveTalentRepository extends JpaRepository<GiveTalent,Long> {
+}
---
+++ src/main/java/com/talearnt/post/exchange/repository/ReceiveTalentRepository.java
@@ -0,0 +1,7 @@
+package com.talearnt.post.exchange.repository;
+
+import com.talearnt.post.exchange.entity.ReceiveTalent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReceiveTalentRepository extends JpaRepository<ReceiveTalent,Long> {
+}
