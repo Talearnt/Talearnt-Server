e591da74c656d44a9a56393c460c1b59199deb6b
Ryu_woong <jin02014@naver.com>
2024-12-18 21:02:09 +0900

    fix : Update WebConfig origins 변경

--- src/main/java/com/talearnt/admin/HealthController.java
+++ src/main/java/com/talearnt/admin/HealthController.java
@@ -1,10 +1,13 @@
 package com.talearnt.admin;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "AWS-ELB", description = "AWS 관련 서버 상태 체크, 개발 사용 X")
 public class HealthController {
 
     @GetMapping("/health")
