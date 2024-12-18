75560d8780f8b4bce1c2ac778df99cda1ffbc871
Ryu_woong <jin02014@naver.com>
2024-12-18 20:33:50 +0900

    chore : Update Webconfig 도메인 경로 추가 Cors 문제 발생

--- src/main/java/com/talearnt/configure/WebConfig.java
+++ src/main/java/com/talearnt/configure/WebConfig.java
@@ -10,7 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://121.168.173.15:5173","http://localhost:5173","http://localhost:5555") // 허용할 도메인
+                .allowedOrigins("http://121.168.173.15:5173",
+                        "http://localhost:5173",
+                        "http://localhost:5555",
+                        "https://api.talearnt.net",
+                        "https://talearnt.net") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true) // 쿠키를 포함한 자격 증명 허용
