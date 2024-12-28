bf0a578466c10a7f4115f69b2e1d5436c424d253
Woong <jin02014@naver.com>
2024-12-28 20:50:42 +0900

    feat : Add 키워드 목록으로 불러오는 방법 테스트 완료

--- src/main/java/com/talearnt/post/exchange/repository/ExchangePostRepository.java
+++ src/main/java/com/talearnt/post/exchange/repository/ExchangePostRepository.java
@@ -1,6 +1,7 @@
 package com.talearnt.post.exchange.repository;
 
 import com.talearnt.post.exchange.entity.ExchangePost;
+import com.talearnt.post.exchange.response.TestListDTO;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,8 +10,18 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ExchangePostRepository extends JpaRepository<ExchangePost, Long> {
+
+    @Query(nativeQuery = true,
+            value = "SELECT JSON_ARRAYAGG(gtc.talent_name) as giveTalents, JSON_ARRAYAGG(rtc.talent_name) as receiveTalents " +
+                    "FROM exchange_post e " +
+                    "LEFT JOIN give_talent gt ON gt.exchange_post_no = e.exchange_post_no " +
+                    "LEFT JOIN receive_talent rt ON rt.exchange_post_no = e.exchange_post_no " +
+                    "LEFT JOIN talent_category gtc ON gtc.talent_code = gt.talent_code " +
+                    "LEFT JOIN talent_category rtc ON rtc.talent_code = rt.talent_code")
+    List<TestListDTO> getTestList();
 
 }
