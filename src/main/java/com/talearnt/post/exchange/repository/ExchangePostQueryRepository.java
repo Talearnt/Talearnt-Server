7d738708e8c1de7b84eb87bdbec27acec07789ef
Ryu_woong <jin02014@naver.com>
2024-12-30 01:27:31 +0900

    feat : Add 재능 게시글 목록 조건 90% 완성
    - 제목 : 제목 검색어가 포함되어 있는 게시글 가져오기 추가 예정 (2글자씩 빼는 DB 기능 사용 예정)
    - Api 문서 작성해야함.

--- src/main/java/com/talearnt/post/exchange/repository/ExchangePostQueryRepository.java
+++ src/main/java/com/talearnt/post/exchange/repository/ExchangePostQueryRepository.java
@@ -1,36 +1,28 @@
 package com.talearnt.post.exchange.repository;
 
 
-import com.querydsl.core.Tuple;
-import com.querydsl.core.group.GroupBy;
-import com.querydsl.core.types.Expression;
-import com.querydsl.core.types.ExpressionUtils;
-import com.querydsl.core.types.Operator;
-import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.*;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.talearnt.admin.category.entity.QTalentCategory;
+import com.talearnt.enums.post.ExchangePostStatus;
+import com.talearnt.enums.post.ExchangeType;
 import com.talearnt.post.exchange.entity.QExchangePost;
 import com.talearnt.post.exchange.entity.QFavoriteExchangePost;
 import com.talearnt.post.exchange.entity.QGiveTalent;
 import com.talearnt.post.exchange.entity.QReceiveTalent;
+import com.talearnt.post.exchange.request.ExchangeSearchConditionDTO;
 import com.talearnt.post.exchange.response.ExchangePostListResDTO;
-import com.talearnt.post.exchange.response.TestListDTO;
 import com.talearnt.user.infomation.entity.QUser;
 import com.talearnt.user.talent.entity.QMyTalent;
-import com.talearnt.util.common.PostUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.hibernate.sql.ast.tree.expression.SqlSelectionExpression;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -65,14 +57,7 @@ public class ExchangePostQueryRepository {
 
 
     /**재능 교환 목록 불러오기 (Filter 조건)<br>*/
-    public List<ExchangePostListResDTO> getFilteredExchangePostList(List<String> categories,
-                                                                    List<String> talents,
-                                                                    String order,
-                                                                    String duration,
-                                                                    String type,
-                                                                    String requiredBadge,
-                                                                    String status,
-                                                                    Pageable page){
+    public List<ExchangePostListResDTO> getFilteredExchangePostList(ExchangeSearchConditionDTO searchConditionDTO){
 
         QTalentCategory talentCategory = QTalentCategory.talentCategory;
         List<ExchangePostListResDTO> result = factory
@@ -102,14 +87,25 @@ public class ExchangePostQueryRepository {
                         ),
                         exchangePost.createdAt,
                         exchangePost.count,
-                        favoriteExchangePost.count().intValue()
+                        favoriteExchangePost.countDistinct().intValue()
                 )).from(exchangePost)
-                        .leftJoin(user).on(exchangePost.user.userNo.eq(user.userNo))
-                        .leftJoin(favoriteExchangePost).on(exchangePost.exchangePostNo.eq(favoriteExchangePost.exchangePostNo))
-                        .leftJoin(giveTalent).on(exchangePost.exchangePostNo.eq(giveTalent.exchangePost.exchangePostNo))
-                        .leftJoin(receiveTalent).on(exchangePost.exchangePostNo.eq(receiveTalent.exchangePost.exchangePostNo))
-                        .groupBy(exchangePost.exchangePostNo)
-                                .fetch();
+                .leftJoin(user).on(exchangePost.user.userNo.eq(user.userNo))
+                .leftJoin(favoriteExchangePost).on(exchangePost.exchangePostNo.eq(favoriteExchangePost.exchangePostNo))
+                .leftJoin(giveTalent).on(exchangePost.exchangePostNo.eq(giveTalent.exchangePost.exchangePostNo))
+                .leftJoin(receiveTalent).on(exchangePost.exchangePostNo.eq(receiveTalent.exchangePost.exchangePostNo))
+                .where(
+                        favoriteExchangePost.deletedAt.isNull(), // 찜 게시글이 삭제되지 않았고, -> favoriteExchangePost를 위함
+                        exchangePost.deletedAt.isNull(), //게시글이 삭제되지 않았고,
+                        filterCategories(searchConditionDTO.getCategories()),//대분류 코드가 일치하고,
+                        filterTalentCodes(searchConditionDTO.getTalents()),//재능 분류의 코드가 일치할 경우
+                        filterDuration(searchConditionDTO.getDuration()),//진행 기간이 일치하고
+                        filterExchangeType(searchConditionDTO.getType()), //진행 방식이 일치하고
+                        filterRequiredBadge(searchConditionDTO.getRequiredBadge()), //인증 뱃지 여부가 일치하고
+                        filterExchangePostStatus(searchConditionDTO.getStatus()) // 모집 상태가 일치하고
+                )
+                .orderBy(filterOrderByOrder(searchConditionDTO.getOrder()).toArray(new OrderSpecifier[0]))// 최신순, 인기순으로 정렬
+                .groupBy(exchangePost.exchangePostNo)
+                .fetch();
 
 
 
@@ -119,16 +115,63 @@ public class ExchangePostQueryRepository {
         return result;
     }
 
-    /** 재능 교환 게시글의 주고 싶은 재능의 대분류가 매개변수 값과 같은 조건 탐색*/
-    private BooleanExpression filterCategories(List<String> categories){
-        List<Integer> result = PostUtil.filterValidIntegers(categories);
-        return !result.isEmpty() ? exchangePost.giveTalents.any().talentCode.bigCategory.categoryCode.in(result) : null;
+    /** 재능 교환 게시글 대분류(categories)가 매개변수 값과 같은 조건 탐색<br>
+     * PostUtil 의 filterValidIntegers 에서 정제를 커친 후 사용함*/
+    private BooleanExpression filterCategories(List<Integer> categories){
+        return !categories.isEmpty() ? exchangePost.giveTalents.any().talentCode.bigCategory.categoryCode.in(categories)
+                .or(exchangePost.receiveTalents.any().talentCode.bigCategory.categoryCode.in(categories)) : null;
     }
 
-    /** 재능 코드 목록을 올바른 값들만 반환하여 조건 탐색*/
-    private BooleanExpression filterTalentCodes(List<String> talents){
-        List<Integer> result = PostUtil.filterValidIntegers(talents);
-        return !result.isEmpty() ? exchangePost.giveTalents.any().talentCode.talentCode.in(result): null;
+    /** 재능 코드 목록(Talents)을 올바른 값들만 반환하여 조건 탐색<br>
+     * PostUtil 의 filterValidIntegers 에서 정제를 커친 후 사용함*/
+    private BooleanExpression filterTalentCodes(List<Integer> talents){
+        return !talents.isEmpty() ? exchangePost.giveTalents.any().talentCode.talentCode.in(talents)
+                .or(exchangePost.receiveTalents.any().talentCode.talentCode.in(talents)): null;
+    }
+
+    /**  정렬 방식(Order) - Order By절 -> 최신순, 인기순<br>
+     * PostUtil 의 filterValidOrderValue 에서 정제를 커친 후 사용함*/
+    private List<OrderSpecifier<?>> filterOrderByOrder(String order){
+        //인기 순일 경우
+        if("popular".equals(order)){
+            LocalDateTime now = LocalDateTime.now();
+            return List.of(
+                    Expressions.cases().when(exchangePost.createdAt.between(now.minusHours(24),now))//24시간 이내로
+                            .then(1)//우선순위 1
+                            .otherwise(2).asc() //나머지는 2순위, then = 1, otherwise = 2 에 대한 .asc
+                    , exchangePost.count.desc() // 게시글 조회수 순
+                    , exchangePost.createdAt.desc()); // 게시글 최신순
+        }
+        // 죄신순일 경우
+        return List.of(exchangePost.createdAt.desc());
+    }
+
+    /** 진행 기간(Duration)에 맞는 게시글 가져오기 <br>
+     * 이미 PostUtil의 filterValidDurationVlaue 에서 정제를 거친 후 사용함<br>
+     * 기간 미정,1개월,2개월,3개월,3개월 이상이 아닌 값은 null*/
+    private BooleanExpression filterDuration(String duration){
+        return duration != null ? exchangePost.duration.eq(duration) : null;
+    }
+
+    /** 재능 교환 진행 방식 (Exchange Type)이 일치하는 게시글 가져오기<br>
+     * PostUtil 의 filterValidExchangeType 에서 정제를 커친 후 사용함<br>
+     * 온라인,오프라인,온_오프라인 외 null 값*/
+    private BooleanExpression filterExchangeType(ExchangeType type){
+        return type != null ? exchangePost.exchangeType.eq(type) : null;
+    }
+
+    /** 인증뱃지 필수 여부 (requiredBadge)<br>
+     * PostUtil 의 filterValidRequiredBadge 에서 정제를 커친 후 사용함<br>
+     * true,false 외 null*/
+    private BooleanExpression filterRequiredBadge(Boolean requiredBadge){
+        return requiredBadge != null ? exchangePost.requiredBadge.eq(requiredBadge) : null;
+    }
+
+    /** 재능 교환 모집 상태 (ExchangePostStatus)<br>
+     * PostUtil 의 filterValidExchangePostStatus 에서 정제를 커친 후 사용함<br>
+     * 모집중,모집_완료 외 null*/
+    private BooleanExpression filterExchangePostStatus(ExchangePostStatus status){
+        return status != null ? exchangePost.status.eq(status) : null;
     }
 
 }
