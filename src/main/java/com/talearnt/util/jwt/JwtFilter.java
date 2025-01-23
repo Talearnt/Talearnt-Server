04043d1464c795febc058cbc6a05a85180ce1c4a
Woong <jin02014@naver.com>
2025-01-23 16:59:07 +0900

    feat : Update JWT 토큰 - Stateless 형태로 변환
    - loadUserByUsername을 Filter 내부에서 직접 호출하여 매번 DB에 들리는 모습이 있었습니다.  이 부분은 Stateless 하지 않기 때문에 Stateless 할 수 있도록 변경하였습니다.

--- src/main/java/com/talearnt/util/jwt/JwtFilter.java
+++ src/main/java/com/talearnt/util/jwt/JwtFilter.java
@@ -2,10 +2,10 @@ package com.talearnt.util.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -16,37 +16,36 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtTokenUtil jwtTokenUtil;
-    private final UserInfoService userInfoService;
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Authorization 헤더에서 JWT 토큰 추출
         String header = request.getHeader("Authorization");
-
-        String jwtToken = null;
-        String userId = null;
-
-
-        //Bearer 에서 토큰 값 추출 후 userId 셋팅
-        if (header != null && header.startsWith("Bearer ")) {
-            jwtToken = header.substring(7);
-            userId = jwtTokenUtil.extractUserId(jwtToken);
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
         }
 
-        // Jwt 토큰은 있지만 JWT Context에 등록이 되어있지 않은 경우
-        if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserInfo userInfo = (UserInfo) userInfoService.loadUserByUsername(userId);
+        // "Bearer " 이후의 토큰 값 추출
+        String jwtToken = header.substring(7);
+        if (jwtToken == null || SecurityContextHolder.getContext().getAuthentication() != null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-            //Jwt 토큰이 유효할 경우 등록
-            if (jwtTokenUtil.isTokenValid(jwtToken, userInfo)) {
-                UsernamePasswordAuthenticationToken authenticationToken =
-                        new UsernamePasswordAuthenticationToken(userInfo, null, userInfo.getAuthorities());
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            }
+        // 토큰 유효성 검사 및 사용자 정보 설정
+        if (jwtTokenUtil.isTokenValid(jwtToken)) {
+            UserInfo userInfo = jwtTokenUtil.extractUserInfo(jwtToken);
+
+            // 인증 객체 생성 및 SecurityContext에 설정
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(userInfo, null, userInfo.getAuthorities());
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         }
 
         filterChain.doFilter(request, response);
--- src/main/java/com/talearnt/util/jwt/JwtTokenUtil.java
+++ src/main/java/com/talearnt/util/jwt/JwtTokenUtil.java
@@ -56,6 +56,20 @@ public class JwtTokenUtil {
                 .getBody();
     }
 
+    // JwtTokenUtil 클래스
+    public UserInfo extractUserInfo(String token) {
+        Claims claims = extractClaims(token); // JWT에서 클레임 추출
+
+        return UserInfo.builder()
+                .userNo(claims.get("userNo", Double.class).longValue()) // userNo 추출 및 Long 변환
+                .userId(claims.getSubject()) // subject는 userId로 설정됨
+                .nickname(claims.get("nickname", String.class)) // nickname 추출
+                .profileImg(claims.get("profileImg", String.class)) // profileImg 추출
+                .authority(UserRole.valueOf(claims.get("authority", String.class)))
+                .build();
+    }
+
+
     // 유저 ID 추출
     public String extractUserId(String token) {
         return extractClaims(token).getSubject();
@@ -68,12 +82,17 @@ public class JwtTokenUtil {
         return UserRole.valueOf(role);
     }
 
-    // 토큰 유효성 확인
+    // 토큰 유효성 확인 - 리프레시 토큰 전용
     public boolean isTokenValid(String token, UserInfo userInfo) {
         String userId = extractUserId(token);
         UserRole authority = extractAuthority(token);
 
         return (userId.equals(userInfo.getUserId()) && authority.equals(userInfo.getAuthority()) && !isTokenExpired(token));
+    }
+
+    // 토큰 유효성 확인 - 어세스 토큰 전용
+    public boolean isTokenValid(String token) {
+        return !isTokenExpired(token);
     }
 
     // 토큰이 만료되었는지 확인하는 메서드
