ee2a18f9c54d8ef050422209ea30cbf16dc8d07e
Woong <jin02014@naver.com>
2025-01-25 15:10:02 +0900

    docs : Update 재능교환 목록
    ### 추가 목록들
    - Query Parameter에 대한 부가 설명
    - Size에 대한 부가 설명
    - 제목 검색에 대한 부가 설명
    - Parameters  categories -> giveTalents , talents -> receiveTalents 로 변경

--- src/main/java/com/talearnt/post/exchange/ExchangePostApi.java
+++ src/main/java/com/talearnt/post/exchange/ExchangePostApi.java
@@ -133,15 +133,21 @@ public interface ExchangePostApi {
 
     @Operation(summary = "재능 교환 게시글 목록"
             , description = "<h2>내용</h2>" +
-            "<p>재능 교환 게시글 목록을 15개 반환합니다.</p>"+
+            "<p>giveTalents, receiveTalents는 List<String>으로 받고 있습니다.</p>"+
+            "<p>잘못된 값이 들어와도 서버에서 오류 발생을 방지하기 위하여 Query Parameter Type이 String으로 구성되어 있습니다.</p>"+
+            "<p>유효한 값이 아닐 경우, 그 값은 제외하고 검색을 시작합니다</p>"+
+            "<p>ex) giveTalents=1001,일공공이 -> 1001만 필터 조건으로 걸림</p>"+
+            "<h3>Size</h3>"+
+            "<p>재능 교환 게시글 목록을 15개(기본) 반환합니다.</p>"+
+            "<p>Size로 원하는 갯수만큼 반환할 수 있습니다.</p>"+
             "<p>Content의 내용에 HTML 태그 제거 및 100자까지 뽑아서 반환합니다.</p>"+
-            "<p>Chatting Rooms DB 구조가 구현되지 않아 지금은 Count - 조회수 를 반환합니다.</p>"+
+            "<h3>제목 검색 (MVP 1차 미사용)</h3>"+
             "<p>제목 검색은 Ngram parser 2 Length를 사용하고 있습니다.</p>"+
             "<p>명확한 검색 결과를 얻기 위해서는 2글자 이상을 검색해야 값이 나옵니다.</p>"+
             "<p>Ngram Parser의 문제점은 띄어쓰기를 포함한 4글자는 검색 결과가 제대로 나오지 않습니다</p>"+
             "<p>ex)네 번째 -> 검색X </p>"+
             "<p>네 번째로 검색시에 번째에 포함되는 게시글을 불러올 수도 있으나 검색에 대한 신뢰도가 하락하는 문제가 있습니다.</p>"+
-            "<p>이 부분에 대해서는 회의가 필요할 듯 합니다.</p>"+
+            "<p>ElasticSearch를 도입하면 검색 결과에 신뢰를 더할 수 있습니다만, 검색이 들어가는 MVP에서 도입을 검토 해야 할 것 같습니다.</p>"+
             "<hr>" +
             "<h2>Response</h2>" +
             "<ul>" +
@@ -156,8 +162,9 @@ public interface ExchangePostApi {
                 "<li>giveTalents : 게시글 주고 싶은 재능</li>" +
                 "<li>receiveTalents : 게시글 받고 싶은 재능</li>" +
                 "<li>createdAt : 게시글 작성일</li>" +
-                "<li>count : 게시글 조회수 -> 추후 채팅방 갯수로 변경</li>" +
+                "<li>openedChatRoomCount : 신청된 채팅방 갯수</li>" +
                 "<li>favoriteCount : 게시글 찜 갯수</li>" +
+                "<li>isFavorite : 게시글 찜 여부</li>" +
                 "<br>" +
                 "<li>hasNext : 다음 버튼 여부</li>" +
                 "<li>hasPrevious : 이전 버튼 여부</li>" +
@@ -165,8 +172,8 @@ public interface ExchangePostApi {
                 "<li>currentPage : 현재 페이지</li>" +
             "</ul>"
     )
-    public ResponseEntity<PaginatedResponse<List<ExchangePostListResDTO>>> getExchangePostList(@RequestParam(value = "categories",required = false,defaultValue = "") @Schema(description = "대분류 - 대분류 코드") List<String> categories,//Integer로 변환 필요
-                                                                                               @RequestParam(value = "talents",required = false,defaultValue = "") @Schema(description = "재능 분류 - 재능 분류 코드") List<String> talents,//Integer로 변환 필요
+    public ResponseEntity<PaginatedResponse<List<ExchangePostListResDTO>>> getExchangePostList(@RequestParam(value = "giveTalents",required = false,defaultValue = "") @Schema(description = "주고 싶은 재능 코드 List") List<String> giveTalents,//Integer로 변환 필요
+                                                                                               @RequestParam(value = "receiveTalents",required = false,defaultValue = "") @Schema(description = "받고 싶은 재능 코드 List") List<String> receiveTalents,//Integer로 변환 필요
                                                                                                @RequestParam(value = "order", required = false,defaultValue = "recent") @Schema(description = "정렬 - recent : 최신순, popular : 인기순") String order,//recent,popular 로 변환 필요
                                                                                                @RequestParam(value = "duration",required = false) @Schema(description = "진행 기간 - 기간 미정, 1개월,2개월,3개월, 3개월 이상 만 가능") String duration,// 이상한 값이 넘어올 경우 duration 없이 조건
                                                                                                @RequestParam(value = "type", required = false) @Schema(description = "진행 방식 - 온라인, 오프라인, 온_오프라인 만 가능") String type, //ExchangeType으로 변환 필요, ExchangeType 으로 변환 실패 시 null로 변환
@@ -174,5 +181,6 @@ public interface ExchangePostApi {
                                                                                                @RequestParam(value = "status",required = false) @Schema(description = "모집 상태 - 모집중, 모집_완료 만 가능") String status, //ExchangePostStatus으로 변환 필요, ExchangePostStatus 으로 변환 실패시  으로 변환 실패 시 null로 변환
                                                                                                @RequestParam(value = "page",required = false,defaultValue = "1") @Schema(description = "기본 1") String page,
                                                                                                @RequestParam(value = "size",required = false,defaultValue = "15") @Schema(description = "입력 X 기본 15개 반환, 필요시 50개 이하 호출 가능, 그 이상 불가능") String size,
-                                                                                               @RequestParam(value = "search",required = false) @Schema(description = "Ngram Parse 사용중, 기본 2글자부터 검색 시 제대로 반환") String search);
+                                                                                               @RequestParam(value = "search",required = false) @Schema(description = "Ngram Parse 사용중, 기본 2글자부터 검색 시 제대로 반환") String search,
+                                                                                               Authentication auth);
 }
