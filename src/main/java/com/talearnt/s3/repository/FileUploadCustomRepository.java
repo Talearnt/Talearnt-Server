9b7a2ffd9b5593d869211bff882589da6be21436
Woong <jin02014@naver.com>
2025-02-10 10:46:38 +0900

    feat : Add 재능교환 게시글에 등록된 이미지 파일 가져오기

---
+++ src/main/java/com/talearnt/s3/repository/FileUploadCustomRepository.java
@@ -0,0 +1,29 @@
+package com.talearnt.s3.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.talearnt.enums.upload.PostType;
+import com.talearnt.s3.entity.FileUpload;
+import com.talearnt.s3.entity.QFileUpload;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+@Log4j2
+public class FileUploadCustomRepository {
+    private final JPAQueryFactory factory;
+    private final QFileUpload fileUpload;
+
+    public List<FileUpload> getFileUploadsByExchangePostNo(Long postNo){
+
+        return factory.selectFrom(fileUpload)
+                .where(fileUpload.postNo.eq(postNo),
+                        fileUpload.postType.eq(PostType.EXCHANGE))
+                .fetch();
+    }
+
+
+}
