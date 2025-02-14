55fbe6e96f546a16fd5d6d86ffe6bd1395893bea
Woong <jin02014@naver.com>
2025-02-14 14:53:53 +0900

    feat : Add 커뮤니티 게시글 엔티티들

---
+++ src/main/java/com/talearnt/post/community/entity/CommunityPost.java
@@ -0,0 +1,49 @@
+package com.talearnt.post.community.entity;
+
+import com.talearnt.enums.upload.PostType;
+import com.talearnt.user.infomation.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommunityPost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long communityPostNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_no")
+    private User user;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,length = 10)
+    private PostType postType;
+
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int count;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
