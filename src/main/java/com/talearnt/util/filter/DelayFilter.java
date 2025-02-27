26ddc6ddd0ca02749f8f2e083a0b94bba29f314d
Ryu_woong <jin02014@naver.com>
2025-02-27 20:40:50 +0900

    feat : Add 지연 유도 Filter
    - 로딩 메세지를 출력 확인을 위한 Filter

---
+++ src/main/java/com/talearnt/util/filter/DelayFilter.java
@@ -0,0 +1,27 @@
+package com.talearnt.util.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+@WebFilter("/*")
+public class DelayFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        filterChain.doFilter(request, response);
+    }
+}
