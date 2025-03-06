80f03a5ef0bccaad2253da81cb6df6dc100379ab
Ryu_woong <jin02014@naver.com>
2025-03-06 16:14:10 +0900

    feat : Update PostType Enum Class Variable
    - enum 클래스에 FE로 넘어갈 때 보여줄 값 추가

--- src/main/java/com/talearnt/enums/post/PostType.java
+++ src/main/java/com/talearnt/enums/post/PostType.java
@@ -6,13 +6,23 @@ import com.talearnt.enums.common.ErrorCode;
 import com.talearnt.util.exception.CustomRuntimeException;
 
 public enum PostType {
-    EXCHANGE,
-    FREE,
-    QUESTION,
-    STUDY;
+    EXCHANGE("재능 교환 게시판"),
+    FREE("자유 게시판"),
+    QUESTION("질문 게시판"),
+    STUDY("스터디 게시판");
+
+    private final String type;
+
+    PostType(String type){this.type = type;}
+
+    @JsonValue
+    public String getType(){
+        return type;
+    }
 
     @JsonCreator
     public static PostType from(String value) {
+        if (value == null || value.isEmpty()) return null;
         for (PostType type : PostType.values()) {
             if (type.name().equalsIgnoreCase(value)) {
                 return type;
