1d536eb28ba03026a9556d8767eff21c301a7b2d
Ryu_woong <jin02014@naver.com>
2025-03-13 02:35:35 +0900

    docs : Update 커뮤니티 게시글 목록 API 문서 모바일 부분 정리 완료

--- src/main/java/com/talearnt/post/community/CommunityPostApi.java
+++ src/main/java/com/talearnt/post/community/CommunityPostApi.java
@@ -23,39 +23,18 @@ public interface CommunityPostApi {
             , description = "<h2>내용</h2>" +
             "<p>커뮤니케이션 목록입니다.</p>" +
             "<p>웹과 모바일의 Response에 차이가 있습니다. Mobile에는 content가 추가됩니다.</p>" +
-            "<p>postType은 (FREE|QUESTION|STUDY)가 있습니다. postType을 넣지 않은 경우는 '전체' 검색입니다.</p>" +
             "<p>path에 (mobile|web) 으로 보내주셔야하고 보내지 않을 경우에는 web으로 기본 설정 되어 있습니다.</p>" +
             "<p>커뮤니티 게시글의 order는 (recent|hot)으로 구성 되어 있고, 기본 값은 recent입니다.</p>" +
-            "<p>baseTime은 첫 목록 호출 시간을 고정 값으로 보내셔야 합니다.</p>" +
-            "<p>baseTime을 설정 안하면 서버에 요청한 시간으로 설정됩니다. (새로운 게시글 갱신될 가능성 있음)</p>" +
-            "<p>ISO_DATE_TIME의 format이고 'yyyy-MM-ddTHH:mm:ss' 형식으로 보내주시면 됩니다.</p>" +
             "<hr/>" +
-            "<h2>최신순 페이지네이션 필요 파라미터 및 조건</h2>"+
-            "<p>최신순 게시물 첫 호출 시에는 order(recent)와 baseTime(첫 목록 호출 시간),path(접근 경로), page(기본값 : 1), size(기본 값: 12) 만 보내주시고</p>" +
-            "<p>두 번째 페이지 요청하실 때 lastNo만 보내주시면 됩니다.</p>" +
-            "<ul>" +
-                "<li>order = recent</li>" +
-                "<li>lastNo = 게시글 마지막 postNo</li>" +
-                "<li>baseTime = 첫 목록 조회 시간 (이후 고정 값 - 새로운 게시글이 포함된 목록을 원할 시 갱신)</li>" +
-                "<li>page = 현재 페이지 번호 + 1</li>" +
-                "<li>size = 호출하고자 하는 게시글 개수</li>" +
-            "</ul>" +
+            "<h2>모바일 참고 내용</h2>" +
+            "<p>첫 게시글 호출 시 path, order, size만 보내고 2페이지 이상 호출 시 lastNo를 추가하시면 됩니다.</p>" +
+            "<p>order : recent(최신순) 과 LastNo : 게시글 마지막 번호를 같이 보내면 최신순 다음 페이지 호출입니다.</p>" +
+            "<p>order : hot(BEST 커뮤니티) 와 LastNo : 게시글 마지막 번호를 같이 보내면 BEST 커뮤니티 게시글 다음 페이지 호출입니다.</p>" +
+            "<p>page는 무조건 1로 보내셔야 합니다.</p>" +
+            "<p>size는 자유입니다. 기본 12로 구현되어 있습니다.</p>" +
             "<hr/>" +
-            "<h2>핫한 게시물 페이지네이션 필요 파라미터 및 조건</h2>"+
-            "<p>핫한 게시물 목록 첫 호출 시에는 order(hot)과 baseTime(첫 목록 호출 시간),path(접근 경로), page(기본값 : 1), size(기본 값: 12) 만 보내주시고</p>" +
-            "<p>두 번째 페이지 요청하실 때 lastNo와 popularScore(마지막 게시글 점수) 를 보내주시면 됩니다.</p>" +
+            "<h2>Response - Mobile</h2>" +
             "<ul>" +
-                "<li>order = hot</li>" +
-                "<li>lastNo = 게시글 마지막 postNo</li>" +
-                "<li>baseTime = 첫 목록 조회 시간 (이후 고정 값 - 새로운 게시글이 포함된 목록을 원할 시 갱신)</li>" +
-                "<li>popularScore = 마지막 인기 점수</li>" +
-                "<li>page = 현재 페이지 번호 + 1</li>" +
-                "<li>size = 호출하고자 하는 게시글 개수</li>" +
-            "</ul>" +
-            "<hr/>" +
-            "<h2>Response</h2>" +
-            "<ul>" +
-                "<li>popularScore : 게시글 인기 점수 (좋아요 * 0.7 + 조회수 * 0.3) </li>" +
                 "<li>profileImg : 작성자의 프로필 이미지 경로</li>" +
                 "<li>nickname : 작성자의 닉네임</li>" +
                 "<li>authority : 작성자의 권한 (인증 유저)</li>" +
@@ -69,10 +48,12 @@ public interface CommunityPostApi {
                 "<li>createdAt : 게시글 작성일</li>" +
                 "<li>content : 게시글 내용(path=mobile 일 경우만 나옴)</li>" +
                 "<br>"+
+
+            "</ul>" +
+            "<hr/>" +
+            "<h2>pagination - Mobile</h2>" +
+            "<ul>" +
                 "<li>hasNext : 다음 게시글 조회 가능 여부</li>" +
-                "<li>hasPrevious : 이전 게시글 조회 가능 여부</li>" +
-                "<li>totalPages : 총 게시글 개수</li>" +
-                "<li>currentPage : 현재 페이지 번호</li>" +
             "</ul>" +
             "")
     @ApiResponses(value = {
