b31fda661c4af6c4123cd64e7969793ee260961a
Woong <jin02014@naver.com>
2025-04-05 15:36:39 +0900

    feat : Add 커뮤니티 Controller - 커뮤니티 답글 목록
    - 커뮤니티 답글 목록
    - WEB | MOBILE 공통 사용 가능
    
    Resovles : #TALEARNT-430

---
+++ src/main/java/com/talearnt/reply/community/ReplyController.java
@@ -0,0 +1,36 @@
+package com.talearnt.reply.community;
+
+import com.talearnt.reply.community.response.ReplyListResDTO;
+import com.talearnt.util.response.CommonResponse;
+import com.talearnt.util.response.PaginatedResponse;
+import com.talearnt.util.version.RestControllerV1;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RestControllerV1
+@Log4j2
+@RequiredArgsConstructor
+@Tag(name = "Reply-Community")
+public class ReplyController {
+
+    private final ReplyService replyService;
+
+    @GetMapping("/replies/communities/{commentNo}")
+    public ResponseEntity<CommonResponse<PaginatedResponse<List<ReplyListResDTO>>>> getReplies(@PathVariable Long commentNo,
+                                                                                               @RequestParam(required = false) String lastNo,
+                                                                                               @RequestParam(required = false, defaultValue = "1") String page,
+                                                                                               @RequestParam(required = false, defaultValue = "10") String size) {
+
+
+        return CommonResponse.success(replyService.getReplies(commentNo, lastNo, page, size));
+    }
+
+
+}
