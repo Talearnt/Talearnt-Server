fe9b6f972dec7ded4fcc95b21478b13b6fd8c9de
Woong <jin02014@naver.com>
2025-04-13 18:21:42 +0900

    feat : Update 지연 기능 비활성화

--- src/main/java/com/talearnt/util/aop/DelayAspect.java
+++ src/main/java/com/talearnt/util/aop/DelayAspect.java
@@ -1,17 +1,16 @@
 package com.talearnt.util.aop;
 
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class DelayAspect {
-    @Before("execution(* com.talearnt.post..*(..)) " +
-            "|| execution(* com.talearnt.user.talent..*(..))"+
-            "|| execution(* com.talearnt.s3..*(..))"
-    )
-    public void delayBeforeMethod() throws InterruptedException {
-        Thread.sleep(700); // 0.7초 지연
-    }
+//    @Before("execution(* com.talearnt.post..*(..)) " +
+//            "|| execution(* com.talearnt.user.talent..*(..))"+
+//            "|| execution(* com.talearnt.s3..*(..))"
+//    )
+//    public void delayBeforeMethod() throws InterruptedException {
+//        Thread.sleep(700); // 0.7초 지연
+//    }
 }
