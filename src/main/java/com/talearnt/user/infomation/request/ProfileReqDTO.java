491c0b9e29931a01e42b7ce369f57119cde1b77b
Woong <jin02014@naver.com>
2025-04-13 18:23:15 +0900

    feat : Add 회원 정보 Request DTO
    Resolves : #TALEARNT-437

---
+++ src/main/java/com/talearnt/user/infomation/request/ProfileReqDTO.java
@@ -0,0 +1,39 @@
+package com.talearnt.user.infomation.request;
+
+import com.talearnt.enums.common.ErrorCode;
+import com.talearnt.enums.common.Regex;
+import com.talearnt.util.common.RequiredJwtValueDTO;
+import com.talearnt.util.jwt.UserInfo;
+import com.talearnt.util.valid.DynamicValid;
+import com.talearnt.util.valid.ListValid;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredJwtValueDTO
+public class ProfileReqDTO {
+    @Schema(hidden = true)
+    private UserInfo userInfo;
+
+    @DynamicValid(errorCode = ErrorCode.USER_NICKNAME_MISMATCH, pattern = Regex.NICKNAME)
+    private String nickname;
+
+    @DynamicValid(errorCode = ErrorCode.FILE_UPLOAD_TYPE_NOT_MATCH, pattern = Regex.FILE_EXTENSION)
+    private String profileImg;
+
+    @ListValid(errorCode = ErrorCode.POST_REQUEST_MISSING, minLength = 1)
+    @ListValid(errorCode = ErrorCode.POST_KEYWORD_LENGTH_MISSING, maxLength = 5)
+    private List<Integer> giveTalents;
+
+    @ListValid(errorCode = ErrorCode.POST_REQUEST_MISSING, minLength = 1)
+    @ListValid(errorCode = ErrorCode.POST_KEYWORD_LENGTH_MISSING, maxLength = 5)
+    private List<Integer> receiveTalents;
+}
