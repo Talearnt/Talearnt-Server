4293144c27b98f505e1f53cf344710962d0ce6d0
Woong <jin02014@naver.com>
2025-04-13 18:19:38 +0900

    feat : Add Repository user 반환 메소드 추가

--- src/main/java/com/talearnt/user/infomation/repository/UserRepository.java
+++ src/main/java/com/talearnt/user/infomation/repository/UserRepository.java
@@ -14,4 +14,5 @@ public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByNickname(String nickname);
     boolean existsByPhone(String phone);
     void deleteByUserId(String userId);
+    Optional<User> findByUserNo(Long userNo);
 }
