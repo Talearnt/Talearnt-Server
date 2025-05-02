8b8f501398e7287a3897978674da93995a376fa3
woong <jin02014@naver.com>
2025-05-02 11:47:27 +0900

    docs : Add 커뮤니티 게시글 좋아요 기능에 대한 문서
    - 비동기 방식으로 작동하는 것을 명시
    - 위험 요소 명시
    - 반환 값에 대한 의견 제시 명시
    
    Resolves : #TALEARNT-442

--- src/main/java/com/talearnt/post/community/CommunityPostApi.java
+++ src/main/java/com/talearnt/post/community/CommunityPostApi.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public interface CommunityPostApi {
 
@@ -173,4 +174,19 @@ public interface CommunityPostApi {
     })
     public ResponseEntity<CommonResponse<Void>> deleteCommunityPost(@PathVariable Long postNo, Authentication authentication);
 
+
+
+    @Operation(summary = "커뮤니티 게시글 좋아요"
+            , description = "<h2>내용</h2>" +
+            "<p>커뮤니티 게시글 좋아요입니다. 토글 방식으로 작동합니다.</p>" +
+            "<p>비동기 방식으로 작동하기 때문에 좋아요 버튼을 눌렀을 경우 1~2초간 서버에 요청 보내는 것을 제어해야 합니다.</p>" +
+            "<p>제어하지 않을 경우 서버에 부화가 생겨 문제가 발생할 수 있습니다.</p>" +
+            "<p>커뮤니티 게시글 좋아요는 반환값이 없습니다.</p>" +
+            "<p>좋아요 개수를 반환할까도 싶었지만, 실시간으로 좋아요 변동이 필요하지 않을 것이라 판단해 프론트 쪽에서 좋아요 개수를 제어해주시면 될 것 같습니다.</p>")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "401",ref="EXPIRED_TOKEN"),
+            @ApiResponse(responseCode = "404",ref="POST_NOT_FOUND")
+    })
+    public ResponseEntity<CommonResponse<CompletableFuture<Void>>> likeCommunityPost(@PathVariable Long postNo, Authentication authentication);
 }
