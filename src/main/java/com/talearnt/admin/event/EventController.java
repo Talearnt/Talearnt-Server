b0ff374326f9bbab7f892b0e5c7dd68bbcb118ee
Ryu_woong <jin02014@naver.com>
2025-07-15 21:12:50 +0900

    feat : Add 초기 이벤트 Controller 설정

---
+++ src/main/java/com/talearnt/admin/event/EventController.java
@@ -0,0 +1,12 @@
+package com.talearnt.admin.event;
+
+import com.talearnt.util.version.RestControllerV1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestControllerV1
+@RequiredArgsConstructor
+@Log4j2
+public class EventController {
+
+}
