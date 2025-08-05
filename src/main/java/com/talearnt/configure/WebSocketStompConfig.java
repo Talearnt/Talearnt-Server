037b95801433f0bc4498a362db580eb8b7343b83
Woong <jin02014@naver.com>
2025-08-05 17:59:52 +0900

    chore : Add 알림 경로 설정 및 Handler

---
+++ src/main/java/com/talearnt/configure/WebSocketStompConfig.java
@@ -0,0 +1,58 @@
+package com.talearnt.configure;
+
+
+import com.talearnt.util.filter.NotificationPreHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final NotificationPreHandler notificationPreHandler;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        /* 메세지 구독(수신) : 접두사로 시작하는 메세지
+            * 예시) /sub/post/1
+            *
+            * 클라이언트에서 메세지를 구독할 때는 "/sub" 접두사를 사용합니다.
+            * 이 접두사는 서버가 메세지를 구독(수신)하는 경로를 나타냅니다.
+            *
+            * 예시) @SubscribeMapping("/post/{id}")
+            *      -> 클라이언트에서 "/sub/post/1"로 메세지를 구독하면 이 메소드가 호출됩니다.
+         */
+        registry.enableSimpleBroker("/sub");
+
+        /* 메세지 전송(발행) : 접두사로 시작하는 메세지
+            * 예시) /pub/post/1
+            *
+            * 클라이언트에서 메세지를 전송할 때는 "/pub" 접두사를 사용합니다.
+            * 이 접두사는 서버가 메세지를 수신하는 경로를 나타냅니다.
+            *
+            * 예시) @MessageMapping("/post/{id}")
+            *      -> 클라이언트에서 "/pub/post/1"로 메세지를 전송하면 이 메소드가 호출됩니다.
+        * */
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(notificationPreHandler);
+    }
+
+}
