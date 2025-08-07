e02f0827edbdb15df04fda9650152f2d32470a1d
Woong <jin02014@naver.com>
2025-08-07 15:56:51 +0900

    feat : Add Notification Repository 기본 설정

---
+++ src/main/java/com/talearnt/stomp/notification/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.talearnt.stomp.notification.repository;
+
+import com.talearnt.stomp.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification,Long> {
+}
