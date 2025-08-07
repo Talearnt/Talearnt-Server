fdb51ec5a57038a93cd40a83dbb0311dca49e66e
Ryu_woong <jin02014@naver.com>
2025-08-07 21:49:27 +0900

    feat : Add @ToString in Notification Entity

--- src/main/java/com/talearnt/stomp/notification/entity/Notification.java
+++ src/main/java/com/talearnt/stomp/notification/entity/Notification.java
@@ -2,10 +2,7 @@ package com.talearnt.stomp.notification.entity;
 
 import com.talearnt.enums.stomp.NotificationType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
