dbc61f66c40291e1c79684b7d41cd8c627ea62a1
Woong <jin02014@naver.com>
2025-08-07 15:56:06 +0900

    feat : Update NotificationResDTO 유저 닉네임, 재능 코드 필드 추가

--- src/main/java/com/talearnt/stomp/notification/response/NotificationResDTO.java
+++ src/main/java/com/talearnt/stomp/notification/response/NotificationResDTO.java
@@ -14,12 +14,11 @@ import java.time.LocalDateTime;
 public class NotificationResDTO {
 
     private Long notificationNo; // 알림 번호
-    private Long senderNo; // 알림을 보낸 사람의 번호
-    private Long receiverNo; // 알림을 받는 사람의 번호
+    private String senderNickname; // 알림을 보낸 사람의 닉네임
     private Long targetNo; // 게시글, 댓글 등 알림의 대상이 되는 엔티티의 번호
     private String content; // 알림 내용
     private NotificationType notificationType; // 알림 타입 (관심 키워드, 댓글 등)
-    private String url; // 알림 클릭 시 이동할 URL
+    private Integer talentCodes; // 재능 코드 (관심 키워드 등에서 사용)
     private Boolean isRead; // 알림 읽음 여부
     private int unreadCount; // 읽지 않은 알림의 개수
     private LocalDateTime createdAt; // 알림 생성 시간
