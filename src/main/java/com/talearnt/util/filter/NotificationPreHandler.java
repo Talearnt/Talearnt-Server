f1558215f21d91c57733272498e7707cc1374e3e
Ryu_woong <jin02014@naver.com>
2025-08-08 00:45:29 +0900

    feat : Remove PreHandler 로그들

--- src/main/java/com/talearnt/util/filter/NotificationPreHandler.java
+++ src/main/java/com/talearnt/util/filter/NotificationPreHandler.java
@@ -13,6 +13,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -33,26 +34,27 @@ public class NotificationPreHandler implements ChannelInterceptor {
     @Override
     public Message<?> preSend(@NotNull Message<?> message, @NotNull MessageChannel channel) {
 
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompHeaderAccessor accessor = MessageHeaderAccessor
+                .getAccessor(message, StompHeaderAccessor.class);
         log.info("WebSocket STOMP Command: {}", accessor.getCommand());
         // CONNECT 프레임일 때만 토큰 검증
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             String authHeader = accessor.getFirstNativeHeader("Authorization");
-            log.info("Authorization Header: {}", authHeader);
+
             if (authHeader != null && authHeader.startsWith(BEARER_PREFIX)) {
                 String token = authHeader.substring(7);
-                log.info("Token: {}", token);
+
                 if (jwtTokenUtil.isTokenValid(token)) {
+
                     String userId = jwtTokenUtil.extractUserId(token);
                     UserInfo userInfo = (UserInfo)userInfoService.loadUserByUsername(userId);
 
-                    log.info("Authenticated UserInfo: {}", userInfo);
-                    log.info("User: {}", userId);
                     Authentication authentication = new UsernamePasswordAuthenticationToken(
                             userInfo, null, userInfo.getAuthorities()
                     );
 
                     accessor.setUser(authentication); // → 이후 @AuthenticationPrincipal 에 사용됨
+
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 } else {
                     log.error("올바르지 않은 인증 토큰 - {} : {}", token,ErrorCode.INVALID_AUTH_CODE);
