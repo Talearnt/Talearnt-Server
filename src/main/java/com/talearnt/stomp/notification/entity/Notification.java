9e30b444a368d8def1291cfd4344cf6f560d129f
Ryu_woong <jin02014@naver.com>
2025-08-12 01:18:28 +0900

    feat : Update 재능 코드 컬럼 추가

--- src/main/java/com/talearnt/stomp/notification/entity/Notification.java
+++ src/main/java/com/talearnt/stomp/notification/entity/Notification.java
@@ -1,11 +1,15 @@
 package com.talearnt.stomp.notification.entity;
 
 import com.talearnt.enums.stomp.NotificationType;
+import com.talearnt.util.converter.notification.IntegerListConverter;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,16 +20,24 @@ import java.time.LocalDateTime;
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "notification_seq")
-    @SequenceGenerator(
+    @SequenceGenerator(name = "notification_seq", sequenceName = "notification_sequence", initialValue = 1, allocationSize = 50)
+    @GenericGenerator(
             name = "notification_seq",
-            sequenceName = "notification_sequence",
-            allocationSize = 50
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "notification_sequence"),
+                    @Parameter(name = "optimizer", value = "none")
+            }
     )
     private Long notificationNo; // 알림 번호
     private Long senderNo; // 알림을 보낸 사람의 번호
     private Long receiverNo; // 알림을 받는 사람의 번호
     private Long targetNo; // 게시글, 댓글 등 알림의 대상이 되는 엔티티의 번호
     private String content; // 알림 내용
+
+    @Convert(converter = IntegerListConverter.class)
+    @Column(columnDefinition = "TEXT")
+    private List<Integer> talentCodes; // 재능 코드 (관심 키워드 등에서 사용)
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
