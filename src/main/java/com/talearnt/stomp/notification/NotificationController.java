61cac59f81526f5d993b1e26af0a25150bb035c6
Ryu_woong <jin02014@naver.com>
2025-08-13 00:24:16 +0900

    feat : Add 알림 읽음 기능 Controller Path 추가

--- src/main/java/com/talearnt/stomp/notification/NotificationController.java
+++ src/main/java/com/talearnt/stomp/notification/NotificationController.java
@@ -2,11 +2,18 @@ package com.talearnt.stomp.notification;
 
 
 import com.talearnt.stomp.notification.response.NotificationResDTO;
+import com.talearnt.util.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /** 알림은 문서를 남길 수 없어서 문서 남기는 대용으로 쓰는 Controller입니다.
  * 이곳에서 이뤄지는 작업은 현재 없습니다.
@@ -19,9 +26,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class NotificationController implements NotificationApi {
 
+    private final NotificationService notificationService;
+
+
     @GetMapping("/queue/notifications")
     public NotificationResDTO dummyNotification(NotificationResDTO notification) {
         return null;
     }
+
+
+    @PutMapping("/notifications/read")
+    public ResponseEntity<CommonResponse<Void>> readNotification(List<Long> notificationNo, Authentication authentication) {
+        notificationService.readNotification(notificationNo, authentication);
+        return CommonResponse.success(null);
+    }
+
+    
 
 }
