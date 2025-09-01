8c12d616038bb495a6b737a9ce5644d93ec22510
Woong <jin02014@naver.com>
2025-09-01 17:15:14 +0900

    feat : Update UserRole (유저 권한) 권한 세분화

--- src/main/java/com/talearnt/enums/user/UserRole.java
+++ src/main/java/com/talearnt/enums/user/UserRole.java
@@ -1,11 +1,25 @@
 package com.talearnt.enums.user;
 
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
-    ROLE_WITHDRAWN,
-    ROLE_SUSPENDED,
-    ROLE_USER,
-    ROLE_AUTHENTICATED,
-    ROLE_MANAGER,
-    ROLE_ADMIN;
+    ROLE_WITHDRAWN(-1),
+    ROLE_SUSPENDED(-1),
+    ROLE_USER(1),
+    ROLE_AUTHENTICATED(2),
+    ROLE_MANAGER(3),
+    ROLE_ADMIN(4);
+
+    private final int level;
+
+    UserRole(int level) {
+        this.level = level;
+    }
+
+    public boolean isHigherOrEqual(UserRole requiredRole) {
+        return this.level >= requiredRole.level;
+    }
+
 }
