ff4e22d969ad6723d4c3f999d8563d2dc710eff2
Woong <jin02014@naver.com>
2025-09-01 17:18:37 +0900

    feat : Add 공지사항 상세 보기 및 작성 기능 서비스

--- src/main/java/com/talearnt/admin/notice/NoticeService.java
+++ src/main/java/com/talearnt/admin/notice/NoticeService.java
@@ -1,11 +1,18 @@
 package com.talearnt.admin.notice;
 
 
+import com.talearnt.admin.notice.entity.Notice;
 import com.talearnt.admin.notice.repository.NoticeQueryRepository;
+import com.talearnt.admin.notice.repository.NoticeRepository;
+import com.talearnt.admin.notice.request.NoticeInsertReqDTO;
+import com.talearnt.admin.notice.response.NoticeDetailResDTO;
 import com.talearnt.admin.notice.response.NoticeListResDTO;
 import com.talearnt.admin.notice.response.NoticeListToWebResDTO;
+import com.talearnt.enums.common.ErrorCode;
+import com.talearnt.enums.user.UserRole;
 import com.talearnt.util.common.PageUtil;
 import com.talearnt.util.common.PostUtil;
+import com.talearnt.util.exception.CustomRuntimeException;
 import com.talearnt.util.pagination.PagedListWrapper;
 import com.talearnt.util.response.PaginatedResponse;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +31,7 @@ public class NoticeService {
 
 
     private final NoticeQueryRepository noticeQueryRepository;
+    private final NoticeRepository noticeRepository;
 
     public PaginatedResponse<List<NoticeListResDTO>> getNoticeList(String path, String page, String size) {
         log.info("공지사항 목록 불러오기 시작 : path - {}, page - {}, size - {}", path, page, size);
@@ -47,6 +55,47 @@ public class NoticeService {
 
         log.info("공지사항 목록 불러오기 완료 - 모바일");
         return new PaginatedResponse<>(result.getContent(), PageUtil.separatePaginationFromEntityToMobile(result));
+    }
+
+    public NoticeDetailResDTO getNoticeDetail(Long noticeNo) {
+        log.info("공지사항 상세 불러오기 시작 : noticeNo - {}", noticeNo);
+
+        Notice notice = noticeRepository.findById(noticeNo).
+                orElseThrow(() -> {
+                    log.error("공지사항 상세 불러오기 실패 - 공지사항 없음 : {}, {}", noticeNo, ErrorCode.POST_NOT_FOUND);
+                    return new CustomRuntimeException(ErrorCode.POST_NOT_FOUND);
+                });
+
+        NoticeDetailResDTO result = NoticeDetailResDTO.builder()
+                .noticeNo(notice.getNoticeNo())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .noticeType(notice.getNoticeType())
+                .createdAt(notice.getCreatedAt())
+                .build();
+
+        log.info("공지사항 상세 불러오기 완료");
+        return result;
+    }
+
+    public void createNotice(NoticeInsertReqDTO noticeInsertReqDTO) {
+        log.info("공지사항 생성 시작 - {}", noticeInsertReqDTO);
+
+        if (!noticeInsertReqDTO.getUserInfo().getAuthority().isHigherOrEqual(UserRole.ROLE_MANAGER)) {
+            log.error("공지사항 생성 실패 - 권한 없음 : {}, {}", noticeInsertReqDTO.getUserInfo().getUserNo(), ErrorCode.ACCESS_DENIED);
+            throw new CustomRuntimeException(ErrorCode.ACCESS_DENIED);
+        }
+
+        Notice notice = new Notice();
+        notice.setTitle(noticeInsertReqDTO.getTitle());
+        notice.setContent(noticeInsertReqDTO.getContent());
+        notice.setNoticeType(noticeInsertReqDTO.getNoticeType());
+        notice.setCreatedBy(noticeInsertReqDTO.getUserInfo().getUserNo());
+
+
+        noticeRepository.save(notice);
+
+        log.info("공지사항 생성 완료");
     }
 
 }
