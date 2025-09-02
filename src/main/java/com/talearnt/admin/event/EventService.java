a2b45d2ad7fa2c91e02ec8e3e84c61ecf9ea524d
Woong <jin02014@naver.com>
2025-09-02 14:53:43 +0900

    feat : Add 이벤트 작성 로직 추가 ( 권한 확인 )

--- src/main/java/com/talearnt/admin/event/EventService.java
+++ src/main/java/com/talearnt/admin/event/EventService.java
@@ -4,13 +4,16 @@ package com.talearnt.admin.event;
 import com.talearnt.admin.event.entity.Event;
 import com.talearnt.admin.event.repository.EventQueryRepository;
 import com.talearnt.admin.event.repository.EventRepository;
+import com.talearnt.admin.event.request.EventInsertReqDTO;
 import com.talearnt.admin.event.response.EventDetailResDTO;
 import com.talearnt.admin.event.response.EventListResDTO;
 import com.talearnt.admin.notice.response.NoticeListResDTO;
 import com.talearnt.enums.common.ErrorCode;
+import com.talearnt.enums.user.UserRole;
 import com.talearnt.util.common.PageUtil;
 import com.talearnt.util.common.PostUtil;
 import com.talearnt.util.exception.CustomRuntimeException;
+import com.talearnt.util.log.LogRunningTime;
 import com.talearnt.util.pagination.PagedListWrapper;
 import com.talearnt.util.response.PaginatedResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +23,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -30,6 +34,8 @@ public class EventService {
     private final EventQueryRepository eventQueryRepository;
     private final EventRepository eventRepository;
 
+
+    @LogRunningTime
     public PaginatedResponse<List<EventListResDTO>> getEventList(String path, String page, String size) {
         log.info("이벤트 목록 불러오기 시작 : path - {}, page - {}, size - {}", path, page, size);
 
@@ -52,6 +58,8 @@ public class EventService {
         return new PaginatedResponse<>(result.getContent(), PageUtil.separatePaginationFromEntityToMobile(result));
     }
 
+
+    @LogRunningTime
     public EventDetailResDTO getEventDetail(Long eventNo) {
         log.info("이벤트 상세 보기 조회 시작 - eventNo : {}", eventNo);
 
@@ -72,6 +80,33 @@ public class EventService {
 
         log.info("이벤트 상세 보기 조회 끝 - result : {}", eventDetailResDTO);
         return eventDetailResDTO;
+    }
+
+    public void createEvent(EventInsertReqDTO insertReqDTO) {
+        log.info("이벤트 작성하기 시작 - insertReqDTO : {}", insertReqDTO);
+
+        //매니저 이상 권한 확인
+        if (!insertReqDTO.getUserInfo().getAuthority().isHigherOrEqual(UserRole.ROLE_MANAGER)){
+            log.error("이벤트 작성 실패 - 권한 없음 : {}, {}", insertReqDTO.getUserInfo().getUserNo(), ErrorCode.ACCESS_DENIED);
+            throw new CustomRuntimeException(ErrorCode.ACCESS_DENIED);
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+
+        Event event = new Event();
+        event.setContent(insertReqDTO.getContent());
+        event.setBannerUrl(insertReqDTO.getBannerUrl());
+        event.setStartDate(insertReqDTO.getStartDate());
+        event.setEndDate(insertReqDTO.getEndDate());
+        event.setCreatedAt(now);
+        event.setIsActive(insertReqDTO.getStartDate() != null &&insertReqDTO.getEndDate() != null &&
+                        !now.isBefore(insertReqDTO.getStartDate()) && now.isBefore(insertReqDTO.getEndDate())
+        );
+        event.setCreatedBy(insertReqDTO.getUserInfo().getUserNo());
+        eventRepository.save(event);
+
+        log.info("이벤트 작성하기 끝");
+
     }
 
 }
