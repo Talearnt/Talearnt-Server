83bcb5dda516fedf35b5fc47b9121a55be527794
Woong <jin02014@naver.com>
2025-09-02 10:54:20 +0900

    feat : Add 이벤트 상세 보기 Response DTO

---
+++ src/main/java/com/talearnt/admin/event/response/EventDetailResDTO.java
@@ -0,0 +1,20 @@
+package com.talearnt.admin.event.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+public class EventDetailResDTO {
+    private Long eventNo;
+    private String content;
+    private String bannerUrl;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private LocalDateTime createdAt;
+    private Boolean isActive;
+}
