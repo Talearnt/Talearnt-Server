6395d0f1080b1bc1c420e9d90487334a1483d551
Ryu_woong <jin02014@naver.com>
2026-03-14 15:41:02 +0900

    fix : Update swagger 문서 경로 변경 후 다시 접근 가능하도록 변경

--- src/main/java/com/talearnt/configure/WebConfig.java
+++ src/main/java/com/talearnt/configure/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -41,6 +42,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public ForwardedHeaderFilter forwardedHeaderFilter() {
         return new ForwardedHeaderFilter();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Swagger UI 정적 리소스 매핑
+        registry.addResourceHandler("/docs/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
